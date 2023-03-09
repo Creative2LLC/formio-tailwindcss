3f336b5f21d94c06211e482077a28ea9d5e5007e
Sam Miller <sam@creative2.com>
2023-03-09 12:12:44 -0500

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   output: {
     library: 'tailwindcss',
     libraryTarget: 'umd',
+    libraryExport: 'default',
     path: path.resolve(__dirname, 'dist'),
     filename: 'tailwindcss.js',
   },
